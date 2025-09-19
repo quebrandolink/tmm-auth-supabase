5ae3d3a7d2b16ca42c9be9fad56397549780ce9a
quebrandolink <quebrandolink@gmail.com>
2025-09-19 15:45:06 -0300

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image";
+
 export default function NotFound() {
     return <div className="text-center m-6" >
-        <img src="/not-found.svg" alt="404" />
+        <Image
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/not-found.svg`}
+            alt="404"
+            width={400}
+            height={300}
+            style={{ display: "inline-block" }}
+        />
         <h2 className="text-4xl font-bold text-gray-900 mb-2">
             Ops!</h2>
         <p className="text-sm font-w300 text-gray-900">A página solicitada não foi encontrada.</p>
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/tmm-auth-supabase" : "",
+  assetPrefix: isProd ? "/tmm-auth-supabase/" : "",
+  images: {
+    unoptimized: true,
+  },
+
   /* config options here */
 };
 
