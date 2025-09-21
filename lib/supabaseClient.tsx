17b4b7abf7d0ca0d07255e3d53cd509a1f7efa35
quebrandolink <quebrandolink@gmail.com>
2025-09-21 19:40:53 -0300

    update

--- lib/supabaseClient.tsx
+++ lib/supabaseClient.tsx
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { createServerClient, serializeCookieHeader } from "@supabase/ssr"
 
 
 export const supabase = createClient(
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
+  output: "export",
   basePath: isProd ? "/tmm-auth-supabase" : "",
   assetPrefix: isProd ? "/tmm-auth-supabase/" : "",
   images: {
